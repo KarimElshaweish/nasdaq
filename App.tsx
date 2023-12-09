4998964cb69f1927a7b037a48c03dac5c5e5d1ad
KarimElshaweish <karimashrafelshaweish@outlook.com>
2023-12-09 07:07:50 +0200

    add ui-kitten

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Button, Text } from 'react-native';
+import { Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {ResourcesNamesList} from './types';
 import { useNavation } from './navigation';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import * as eva from '@eva-design/eva';
 
 const HomeScreen = ()=>{
   const navigation = useNavation();
@@ -17,7 +19,9 @@ const HomeScreen = ()=>{
 const DetailsScreen = ()=>{
   return (
     <SafeAreaView>
-      <Text>Details Screen</Text>
+      <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text category='h1'>Detils</Text>
+  </Layout>
   </SafeAreaView>
   )
 }
@@ -25,12 +29,14 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    <ApplicationProvider {...eva} theme={eva.dark}>
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name={ResourcesNamesList.HOME} component={HomeScreen} options={{headerShown:false}}/>
         <Stack.Screen name={ResourcesNamesList.DETAILS} component={DetailsScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
+     </ApplicationProvider>
    
   );
 }
